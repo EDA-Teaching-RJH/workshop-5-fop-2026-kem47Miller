10cb5387f66c363d157ce48c65282d0e64b91ee0
kem47 <kem47@kent.ac.uk>
2026-02-02 14:24:53 +0000

    www

--- coffee.py
+++ coffee.py
@@ -1,1 +1,19 @@
-#Your code goes here
+cost = 75
+payment = 0
+valid_coins = [5, 10, 20, 50]
+
+while payment < cost:
+    coin = int(input("Give coin (5, 10, 20, 50): ").replace("£", ""))
+
+    if coin in valid_coins:
+        payment += coin
+        if payment < cost:
+            print(f"Amount owed: £{cost - payment}")
+    else:
+        print("Invalid coin")
+
+change = payment - cost
+if change > 0:
+    print(f"Change: £{change}")
+else:
+    print("Amount paid.Thank you!")
