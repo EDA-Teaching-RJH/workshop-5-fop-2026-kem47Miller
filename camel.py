008d4fee9f226708775c1475030de3a371d2155f
kem47 <kem47@kent.ac.uk>
2026-02-02 13:09:31 +0000

    hth

--- camel.py
+++ camel.py
@@ -1,1 +1,8 @@
-# Enter your code here
+name=str(input("What is your name in camelCase? "))
+snake_case=""
+for char in name:
+    if char.isupper():
+        snake_case += '_' + char.lower()
+    else:
+        snake_case += char
+print(snake_case)
